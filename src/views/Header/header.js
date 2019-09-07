9b8e19645b19c3d5aaf22ca32ba1b0764ed1d043
Rohil Gupta <rohilvg@gmail.com>
2019-09-07 06:49:25 -0400

    deleted frontend component

--- src/views/Header/header.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import { Link } from 'react-router-dom';
-
-export default function Header({ match }) {
-    return <h1>Header</h1>;
-}
