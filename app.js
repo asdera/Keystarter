16e419b365bf26e5263030e231f84be4fcf7e1c8
Rohil Gupta <rohilvg@gmail.com>
2019-09-07 20:46:19 -0400

    Small change

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ var url =
 const Bot = require("keybase-bot");
 
 const bot = new Bot();
-const companies = ['netflix', 'hulu','pandora','headspace', 'apple music', 'google play', 'spotify', 'luminosity', 'amazon prime', 'deezer', 'grammarly', 'brilliant']
+const companies = [
+  "netflix",
+  "hulu",
+  "pandora",
+  "headspace",
+  "apple music",
+  "google play",
+  "spotify",
+  "luminosity",
+  "amazon prime",
+  "deezer",
+  "grammarly",
+  "brilliant"
+];
 
 const username = "ourlovelybot";
 const paperkey =
@@ -28,23 +41,24 @@ bot
       const timestamp = messageContent.sentAt;
       var company;
 
-      for (var i = 0; i < companies.length; i++){
-        if (plaintext.includes(companies[i])){
-          company = companies[i]
+      console.log(plainText);
+
+      for (var i = 0; i < companies.length; i++) {
+        if (plaintext.includes(companies[i])) {
+          company = companies[i];
+          console.log("found company", company);
           break;
         }
       }
 
       MongoClient.connect(url, function(err, db) {
-      // Check if 1. subscription exists in collection subscription_count
-      if (err) throw err;
-      var myobj = { subscription: company};
-      db.collection("subscription_count")
-        .find()
-        .toArray()
-        .then(items =>
-          checkIfExists(items, db, myobj)
-        );
+        // Check if 1. subscription exists in collection subscription_count
+        if (err) throw err;
+        var myobj = { subscription: company };
+        db.collection("subscription_count")
+          .find()
+          .toArray()
+          .then(items => checkIfExists(items, db, myobj));
       });
 
       const addSubscription =
@@ -138,58 +152,65 @@ function insertdbCallback(retArr, channel, timestamp, incoming_username, db) {
 }
 
 function checkIfExists(retArr, db, myobj) {
-  var number_of_people = 0
+  var number_of_people = 0;
   //add number
-  retArr.forEach((collection) => {
+  retArr.forEach(collection => {
     console.log(collection);
-    if (collection.subscription == myobj.subscription){
+    if (collection.subscription == myobj.subscription) {
       number_of_people = collection.number;
     }
-  })
+  });
   var subscription = myobj.subscription;
   //keeps a count of where you in the list you are
   var temp_subcount = 0;
   //subscription doesn't exist
   if ((retArr && retArr.length === 0) || !retArr) {
-    console.log("Subscription doesn't exist")
+    console.log("Subscription doesn't exist");
   } else {
     db.listCollections().toArray(function(err, collInfos) {
-    // collInfos is an array of collection info objects that look like:
+      // collInfos is an array of collection info objects that look like:
       console.log(collInfos);
 
-      collInfos.forEach((collection) => {
+      collInfos.forEach(collection => {
         console.log(collection);
         var name = collection.name;
         console.log(name);
-        if (name.includes(subscription) && name.includes("#")){
-
+        if (name.includes(subscription) && name.includes("#")) {
           var temp = parseInt(name.split("#")[1]);
-          if (temp_subcount <= temp){
+          if (temp_subcount <= temp) {
             temp_subcount = temp;
           }
         }
-      })
+      });
 
       // check if temp_subcount is 0 if 0 then we init a new collection with format "<subscription>#1"
-      if (temp_subcount === 0){
-        var collection_name = subscription+"#1"
+      if (temp_subcount === 0) {
+        var collection_name = subscription + "#1";
         db.createCollection(collection_name, function(err, res) {
           if (err) throw err;
           console.log("Collection created : ", collection_name);
         });
-      } else{
-          var sub = (subscription+"#"+temp_subcount);
-          db.collection(sub).find().toArray(function(err, items) {
-              console.log("Items : ", items);
-              console.log("Len(Items) : ", items.length);
-              if (items.length >= number_of_people){
-                db.createCollection((subscription+"#"+(temp_subcount+1)), function(err, res) {
+      } else {
+        var sub = subscription + "#" + temp_subcount;
+        db.collection(sub)
+          .find()
+          .toArray(function(err, items) {
+            console.log("Items : ", items);
+            console.log("Len(Items) : ", items.length);
+            if (items.length >= number_of_people) {
+              db.createCollection(
+                subscription + "#" + (temp_subcount + 1),
+                function(err, res) {
                   if (err) throw err;
-                  console.log("Collection created : ", (subscription+"#"+(temp_subcount+1)));
-                });
-              }
+                  console.log(
+                    "Collection created : ",
+                    subscription + "#" + (temp_subcount + 1)
+                  );
+                }
+              );
+            }
           });
-        }
+      }
     });
   }
 }
